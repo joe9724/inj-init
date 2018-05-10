5544a865ea1250ca1da04694180e8c4fac9098ab
PF <64060764@qq.com>
2018-05-10 14:16:31 +0800

    fd

--- main.go
+++ main.go
@@ -1,4 +1,96 @@
 package main
 
+import (
+	"net/http"
+	"log"
+	_"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+	"encoding/json"
+	"inj-init/utils"
+	"inj-init/model"
+)
+
+
 func main() {
+	http.HandleFunc("/startUp", StartUp)
+	http.HandleFunc("/home", Home)
+	err := http.ListenAndServe(":106", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+	fmt.Println("start server at 0.0.0.0:106")
+
+}
+
+func StartUp(w http.ResponseWriter, req *http.Request) {
+	db,err := utils.OpenConnection()
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	defer db.Close()
+	var start model.StartUpModel
+	db.Raw("select cover,`force`,number,msg,downloadUrl as download_url from btk_Version where client=?",req.URL.Query().Get("client")).First(&start)
+	fmt.Println("start is",start)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Server", "A Go Web Server")
+	data, err := json.Marshal(start)
+	if err != nil {
+		log.Fatal("err get data: ", err)
+	}
+
+	w.Write(data)
+
+
+}
+
+func Home(w http.ResponseWriter, req *http.Request) {
+	db,err := utils.OpenConnection()
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	defer db.Close()
+	var home model.Home
+	//get banners
+	var banners []model.Banner
+	db.Raw("select id,title,type,cover,target_id,web_url from btk_Banner").Find(&banners)
+	fmt.Println("banners is",banners)
+	//get icons
+	var icons []model.Icon
+	db.Table("btk_Icon").Select("id, title,icon,target_id,web_url,type").Find(&icons)
+	fmt.Println("icons is",icons)
+	var cityToutiao model.CityToutiao
+	cityToutiao.Icon = ""
+	cityToutiao.Cover = ""
+	cityToutiao.Title = "xxx"
+	cityToutiao.Type = 1
+	cityToutiao.TargetID = 1
+	cityToutiao.WebURL = "bbb"
+	fmt.Println("cityToutiao is",cityToutiao)
+	//get zones
+	var zones []model.ZoneItem
+	db.Raw("SELECT ZoneID as zone_id, Name as name, Logo as logo, Brief as brief, MemberCount as member_count, Level as level, Tag as tag, CreateAt as create_at FROM btk_Zone WHERE Status = 0 ORDER BY CreateAt DESC").Limit(6).Offset(0).Find(&zones)
+	fmt.Println("zones is",zones)
+	//get goods 待开发
+	//get activity
+	var activities []model.Activity
+    db.Raw("select event_id,event_title,event_summary,event_thumb from t_event limit 0,4").Find(&activities)
+    fmt.Println("activies is",activities)
+    //get news
+    var news []model.News
+    db.Raw("select id,thumb,title,source,create_time,web_url from btk_News limit 0,6").Find(&news)
+    fmt.Println("news is",news)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Server", "A Go Web Server")
+	home.Banners = banners
+	home.Zones = zones
+	home.Icons = icons
+	home.CityTT = cityToutiao
+	data, err := json.Marshal(home)
+	if err != nil {
+		log.Fatal("err get data: ", err)
+	}
+
+	w.Write(data)
 }
---
+++ model/model.go
@@ -0,0 +1,136 @@
+package model
+
+//启动页model
+type StartUpModel struct{
+	// download Url
+	DownloadURL string `json:"downloadUrl,omitempty"`
+	// force
+	Force string `json:"force,omitempty"`
+
+	// msg
+	Msg string `json:"msg,omitempty"`
+
+	// number
+	Number float64 `json:"number,omitempty"`
+
+	Cover string `json:"cover,omitempty"`
+
+}
+
+//首页model细分
+//Banner
+type Banner struct {
+	// id
+	ID int64 `json:"id"`
+
+	Title string `json:"title"`
+	// cover
+	Cover string `json:"cover"`
+	// type
+	Type int64 `json:"type"`
+	// 跳转类型(0=圈子 1=帖子 2=用户 3=活动 4=新闻 5=web)
+
+	TargetID int64 `json:"target_id"`
+
+	WebUrl string `json:"webUrl"`
+
+}
+type Icon struct {
+	// icon
+	Icon string `json:"icon"`
+
+	// id
+	ID int64 `json:"id"`
+
+	// name
+	Title string `json:"title"`
+
+	// order
+	Order int64 `json:"order"`
+
+	// 跳转类型(0=圈子 1=帖子 2=用户 3=活动 4=新闻 5=web)
+	Type int64 `json:"type"`
+
+	// web Url
+	WebURL string `json:"web_url"`
+
+	TargetID int64 `json:"target_id"`
+}
+
+type CityToutiao struct {
+	Cover string `json:"cover"`
+	// icon
+	Icon string `json:"icon"`
+
+	// id
+	ID int64 `json:"id"`
+
+	// name
+	Title string `json:"title"`
+
+	// order
+	Order int64 `json:"order"`
+
+	// 跳转类型(0=圈子 1=帖子 2=用户 3=活动 4=新闻 5=web)
+	Type int64 `json:"type"`
+
+	// web Url
+	WebURL string `json:"web_url"`
+
+	TargetID int64 `json:"target_id"`
+}
+type ZoneItem struct {
+
+	// 圈子简介
+	Brief string `json:"brief"`
+
+	// 创建日期
+	CreateAt string `json:"create_at"`
+
+	// 是否加入该圈子
+	Joind bool `json:"joind,omitempty"`
+
+	// 圈子等级
+	Level int64 `json:"level"`
+
+	// 圈子的缩略图
+	// Required: true
+	Logo *string `json:"logo"`
+
+	// 圈子的成员个数
+	MemberCount int64 `json:"member_count"`
+
+	// 圈子名称
+	// Required: true
+	Name *string `json:"name"`
+
+	// 圈子状态
+	Status int64 `json:"status"`
+
+	// 圈子的标签
+	Tag string `json:"tag"`
+
+	// 圈子唯一标识符
+	// Required: true
+	ZoneID int64 `json:"zone_id"`
+}
+type Activity struct{
+	EventThumb string `json:"event_thumb"`
+	EventTitle string `json:"event_title"`
+	EventSummary string `json:"event_summary"`
+	EventID string `json:"event_id"`
+}
+type News struct{
+	ID int64 `json:"id"`
+	Thumb string `json:"thumb"`
+	Title string `json:"title"`
+	Source string `json:"source"`
+	CreateTime string `json:"create_time"`
+	WebUrl string `json:"web_url"`
+}
+type Home struct{
+	Banners []Banner `json:"banners"`
+	Icons []Icon `json:"icons"`
+	CityTT CityToutiao `json:"cityToutiao"`
+	Zones []ZoneItem `json:"zones"`
+}
